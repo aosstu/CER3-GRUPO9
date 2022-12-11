f9c98a447c57e76868e43dfd257c47cf81d9503c
CarlitoxDG <112353262+CarlitoxDG@users.noreply.github.com>
2022-12-11 00:53:28 -0300

    Add files via upload
    Implementación de modelos

---
+++ CER3 V2/Hospital/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Bebe,AvanceDiario
+# Register your models here.
+
+@admin.register(Bebe)
+class BebeCoordinadora(admin.ModelAdmin):
+    list_display=('nombre','apellido_paterno','madre','sexo')
+@admin.register(AvanceDiario)
+class AvanceDiarioBebe(admin.ModelAdmin):
+    list_display=('bebe','fecha')
+    
